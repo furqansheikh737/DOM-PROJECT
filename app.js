eec2e2de639682c1ed9017f527cbfe73ff50060e
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2023-09-02 11:42:22 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,100 @@
+ /* First project */
+
+
+//let istatus = document.querySelector("h5");
+// let btn = document.querySelector("#add");
+// flag = 0
+// btn.addEventListener("click", function(){
+//     if (flag === 0) {
+//         istatus.innerHTML = ("Friend")
+//         istatus.style.color = "green"
+//         console.log("hui hui");
+//         btn.innerHTML = ("Remove Friend")
+//         flag = 1
+//     }else{
+//         istatus.innerHTML = ("Strenger")
+//         istatus.style.color = "red"
+//         console.log("nh hui");
+//         btn.innerHTML = ("Add Friend")
+//         flag = 0
+//     }
+
+// })
+
+/* second project */
+
+// let con = document.querySelector(".container");
+// let ilove = document.querySelector("i");
+
+// con.addEventListener("dblclick",function () {
+//     ilove.style.transform = "translate(-50%, -50%) scale(1)";
+//     ilove.style.opacity = 0.7;
+//     setTimeout(() => { 
+//         ilove.style.opacity = 0;
+//       }, 1500);
+//     setTimeout(() => { 
+//       ilove.style.transform = "translate(-50%, -50%) scale(0)";
+//     }, 2000);
+// });
+
+/* Third project */
+
+// let main = document.querySelector(".main")
+// let cursor = document.querySelector(".cursor")
+
+// main.addEventListener("mousemove", function(dets){
+//   cursor.style.left = dets.x+"px"
+//   cursor.style.top = dets.y+"px"
+// })
+
+/* Forth project */
+
+// let elem =  document.querySelectorAll(".elem");
+
+
+// elem.forEach(function(val){
+
+//     val.addEventListener("mouseenter", function(){
+//         val.childNodes[3].style.opacity = 1
+//     });
+//     val.addEventListener("mouseleave", function(){
+//         val.childNodes[3].style.opacity = 0   
+//     });
+//     val.addEventListener("mousemove", function(dets){
+//         val.childNodes[3].style.left = dets.x+"px"
+//     });
+// });
+
+
+/* Firth project */
+
+
+// let arr  = [
+//     {dp:"https://plus.unsplash.com/premium_photo-1692340972716-f2e08765ef90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1692035053386-76667a66067a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"},
+//     {dp:"https://images.unsplash.com/photo-1689697247482-c8cb53d8f670?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDV8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1681474936032-65e35149b123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"},
+//     {dp:"https://images.unsplash.com/photo-1692789183372-1b4b9f73a5ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1691655245135-cccad4661ba8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDYzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"},
+//     {dp:"https://plus.unsplash.com/premium_photo-1690366910332-6cb1dc10c000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1692459525014-596d53e68329?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"},
+//     {dp:"https://images.unsplash.com/photo-1692178574211-5ebf8c120f3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ0fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1687201659725-38e536473e3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUyfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"},
+//     {dp:"https://images.unsplash.com/photo-1692266107806-7044c11dc5f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU5fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60", story:"https://images.unsplash.com/photo-1689613188308-ef2815f15810?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDcwfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"}
+// ];
+
+
+// let storiyan = document.querySelector(".storiyan")
+// let flag = "";
+// arr.forEach(function(elem, index){
+//     flag += `<div class="story">
+//     <img id="${index}" src="${elem.dp}" alt="">
+//    </div>`
+// })
+
+// storiyan.innerHTML = flag;
+
+// storiyan.addEventListener("click", function(dets){
+//    document.querySelector(".screen").style.display = "block"
+//    document.querySelector(".screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+// })
+
+// setTimeout(() => {
+//     document.querySelector(".screen").style.display = "none"
+
+// }, 3000);
